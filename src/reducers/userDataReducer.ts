d70072a4bfb322599e0d527c8f0b5f6c429407fd
Luke Hatcher <lukehatcher98@gmail.com>
2021-02-20 18:55:50 -0800

    working todo deletion

--- src/reducers/userDataReducer.ts
+++ src/reducers/userDataReducer.ts
@@ -1,44 +1,54 @@
 const initialState = []; // array of objs, where each obj has todos arry of objs
 // https://stackoverflow.com/questions/6854431/how-do-i-get-the-objectid-after-i-save-an-object-in-mongoose/47002504
 
-interface userDataShape {
+interface userData {
   _id: string;
   projectName: string;
   todos: any[];
 }
 
-export const UserDataReducer = (state = initialState, action): userDataShape[] => {
-  switch (action.type) {
+export const UserDataReducer = (state = initialState, action): userData[] => {
+  const { type, payload } = action; // need to add destructuring in other files
+  switch (type) {
     case 'fetchData':
-      return action.payload.projects;
+      return payload.projects;
     case 'addProject':
       return [
         ...state,
         {
-          _id: Math.random(), // needs to match db id!!!!!!!
-          projectName: action.payload,
+          _id: Math.random(), // should match db id!!!!!!!
+          projectName: payload,
           todos: [],
         },
       ];
     case 'deleteProject':
-      return state.filter((i) => i.projectName !== action.payload);
+      return state.filter((i) => i.projectName !== payload);
     case 'addTodo':
-      // looks commplicated cause we need to copy each level!
+      // looks complicated cause we need to copy each level!
       return state.map((item) => {
-        if (item.projectName !== action.payload.projectName) {
+        if (item.projectName !== payload.projectName) {
           return { ...item };
         } else {
           return {
             ...item,
             todos: [
               ...item.todos,
-              { _id: Math.random(), text: action.payload.todo, completed: false }, // wrong id
+              { _id: Math.random(), text: payload.todo, completed: false }, // wrong id
             ],
           };
         }
       });
-    // console.log(newState);
-    // return newState;
+    case 'deleteTodo':
+      return state.map((item) => {
+        if (item.projectName !== payload.projectName) {
+          return { ...item };
+        } else {
+          return {
+            ...item,
+            todos: item.todos.filter((todo) => todo.text !== payload.todo),
+          };
+        }
+      });
     default:
       return state;
   }
