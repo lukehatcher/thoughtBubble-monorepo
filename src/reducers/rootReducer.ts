fb4f72389054e5a3d5dcd5da2eabc1c86d776163
Luke Hatcher <lukehatcher98@gmail.com>
2021-02-21 23:07:52 -0800

    comment docs

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -4,10 +4,9 @@ import { storeUserReducer } from './storedUserReducer';
 import { UserDataReducer } from './userDataReducer';
 
 export const rootReducer = combineReducers({
-  // Define a top-level state field named `loginStatus`, handled by `loginStatusReducer`
   loginStatus: loginStatusReducer,
   storedUser: storeUserReducer,
   userData: UserDataReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>; // used for useSelector
+export type RootState = ReturnType<typeof rootReducer>; // used for useSelector hooks
