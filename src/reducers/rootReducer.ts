318cf2650ce0d9a990ab0b1691b080614f7311c1
Luke Hatcher <lukehatcher98@gmail.com>
2021-02-23 00:53:49 -0800

    empties the redux store on logout without having to reload app

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -2,9 +2,18 @@ import { combineReducers } from 'redux';
 import { storeUserReducer } from './storedUserReducer';
 import { UserDataReducer } from './userDataReducer';
 
-export const rootReducer = combineReducers({
+const appReducer = combineReducers({
   storedUser: storeUserReducer,
   userData: UserDataReducer,
 });
+
+export const rootReducer = (state, action) => {
+  if (action.type === 'USER_LOGOUT') {
+    // https://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store
+    // reducers are supposed to return the initial state when they are called with undefined
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 export type RootState = ReturnType<typeof rootReducer>; // used for useSelector hooks
