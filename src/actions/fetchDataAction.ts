ded77d7bdd6b939659aab9819304413dd9301b8d
Luke Hatcher <lukehatcher98@gmail.com>
2021-02-24 02:23:48 -0800

    added param type

--- src/actions/fetchDataAction.ts
+++ src/actions/fetchDataAction.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const fetchDataAction = (userSub) => {
+export const fetchDataAction = (userSub: string) => {
   return async (dispatch) => {
     try {
       const response = await axios.get('http://localhost:3001/api/projects/fetch', {
