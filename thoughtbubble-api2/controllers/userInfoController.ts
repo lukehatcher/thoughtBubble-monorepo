df5804e9632992f284254396bfc8d93845514be0
Luke Hatcher <lukehatcher98@gmail.com>
2021-04-20 14:02:03 -0700

    add weekly email and darkmode settings to entities and controllers

--- thoughtbubble-api2/controllers/userInfoController.ts
+++ thoughtbubble-api2/controllers/userInfoController.ts
@@ -3,47 +3,14 @@ import { Request, Response } from 'express';
 import { getConnection } from 'typeorm';
 import { User } from '../entities/User';
 import { sendEmail } from '../services/email';
+import { youtubeAnalytics } from 'googleapis/build/src/apis/youtubeAnalytics';
 
 class userInfoController {
   private readonly location: string;
 
   constructor() {
-    this.location = '@emailControllers.ts: ';
+    this.location = '@userInfoControllers.ts: ';
   }
-
-  public toggleDailyEmailSetting = async (req: Request, res: Response): Promise<void> => {
-    const { userSub } = req.body;
-    try {
-      const user = await User.findOne({ githubId: userSub });
-      const dailyEmailSetting = user?.dailyEmail;
-      if (dailyEmailSetting) {
-        // opting out
-        sendEmail(
-          user!.email,
-          'thoughtBubble email settings changed',
-          '<h1>thoughtBubble</h1><p>you have successfully turned <b>off</b> email notifications</p>'
-        );
-        res.sendStatus(200);
-      } else {
-        // opting in
-        sendEmail(
-          user!.email,
-          'thoughtBubble email settings changed',
-          '<h3>thoughtBubble</h3><p>you have successfully turned <b>on</b> email notifications</p>'
-        );
-        await getConnection() //
-          .createQueryBuilder()
-          .update(User)
-          .set({ dailyEmail: !dailyEmailSetting })
-          .where('githubId = :githubId', { githubId: userSub })
-          .execute();
-        res.sendStatus(200);
-      }
-    } catch (err) {
-      console.error(this.location, err);
-      res.sendStatus(400);
-    }
-  };
 
   public fetchUserInfo = async (req: Request, res: Response): Promise<void> => {
     const userSub = req.query.userSub as string;
@@ -63,6 +30,95 @@ class userInfoController {
       console.error(this.location, err);
       res.sendStatus(400);
     }
+  };
+
+  public toggleDarkMode = async (req: Request, res: Response) => {
+    const { userSub } = req.body;
+    const user = await User.findOne({ githubId: userSub });
+    const darkModeSetting = user?.darkMode;
+    await getConnection() //
+      .createQueryBuilder()
+      .update(User)
+      .set({ darkMode: !darkModeSetting })
+      .where('githubId = :githubId', { githubId: userSub })
+      .execute();
+    res.sendStatus(200);
+  };
+
+  public toggleDailyEmailSetting = async (req: Request, res: Response): Promise<void> => {
+    const { userSub } = req.body;
+    const user = await User.findOne({ githubId: userSub });
+    const dailyEmailSetting = user?.dailyEmail;
+    if (dailyEmailSetting) {
+      // opting out
+      try {
+        sendEmail(
+          user!.email,
+          'thoughtBubble email settings changed',
+          '<h1>thoughtBubble</h1><p>you have successfully turned <b>off</b> daily email notifications</p>'
+        );
+      } catch (err) {
+        console.error(this.location, err);
+        res.sendStatus(400);
+      }
+    } else {
+      // opting in
+      try {
+        sendEmail(
+          user!.email,
+          'thoughtBubble email settings changed',
+          '<h3>thoughtBubble</h3><p>you have successfully turned <b>on</b> daily email notifications</p>'
+        );
+      } catch (err) {
+        console.error(this.location, err);
+        res.sendStatus(400);
+      }
+    }
+    await getConnection() //
+      .createQueryBuilder()
+      .update(User)
+      .set({ dailyEmail: !dailyEmailSetting })
+      .where('githubId = :githubId', { githubId: userSub })
+      .execute();
+    res.sendStatus(200);
+  };
+
+  public toggleWeeklyEmailSetting = async (req: Request, res: Response): Promise<void> => {
+    const { userSub } = req.body;
+    const user = await User.findOne({ githubId: userSub });
+    const weeklyEmailSetting = user?.weeklyEmail;
+    if (weeklyEmailSetting) {
+      // opting out
+      try {
+        sendEmail(
+          user!.email,
+          'thoughtBubble email settings changed',
+          '<h1>thoughtBubble</h1><p>you have successfully turned <b>off</b> weekly email notifications</p>'
+        );
+      } catch (err) {
+        console.error(this.location, err);
+        res.sendStatus(400);
+      }
+    } else {
+      // opting in
+      try {
+        sendEmail(
+          user!.email,
+          'thoughtBubble email settings changed',
+          '<h3>thoughtBubble</h3><p>you have successfully turned <b>on</b> weekly email notifications</p>'
+        );
+      } catch (err) {
+        console.error(this.location, err);
+        res.sendStatus(400);
+      }
+    }
+    await getConnection() //
+      .createQueryBuilder()
+      .update(User)
+      .set({ weeklyEmail: !weeklyEmailSetting })
+      .where('githubId = :githubId', { githubId: userSub })
+      .execute();
+    res.sendStatus(200);
   };
 }
 
