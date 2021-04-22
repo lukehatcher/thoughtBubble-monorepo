b9a7c233a7ad18f973b4a943d157055abdc973e4
Luke Hatcher <lukehatcher98@gmail.com>
2021-04-22 01:03:25 -0700

    update initial state of users token interface

--- thoughtbubble-mobile/src/reducers/storedUserReducer.ts
+++ thoughtbubble-mobile/src/reducers/storedUserReducer.ts
@@ -1,6 +1,22 @@
 import { idTokenShape } from '../interfaces/token';
 
-const initialState = null;
+const initialState = {
+  aud: '',
+  auth_time: '',
+  email: '',
+  email_verification: true,
+  exp: 0,
+  family_name: '',
+  given_name: '',
+  iat: 0,
+  iss: '',
+  locale: '',
+  name: '',
+  nickname: '',
+  picture: '',
+  sub: '',
+  updated_at: '',
+};
 
 export const storeUserReducer = (state = initialState, action): idTokenShape => {
   switch (action.type) {
