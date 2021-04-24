d81a923f5111aafc9acff81078eb6dbd69e09eee
Luke Hatcher <lukehatcher98@gmail.com>
2021-04-24 03:11:56 -0700

    correct /tag route controller bug/typos

--- thoughtbubble-api2/routes/thoughts.ts
+++ thoughtbubble-api2/routes/thoughts.ts
@@ -3,16 +3,18 @@ import ThoughtsController from '../controllers/thoughtController';
 
 const router = express.Router();
 
-// api/thought
-router
+router // api/thought
   .route('/')
   .post(ThoughtsController.createThought)
   .put(ThoughtsController.editThought)
   .delete(ThoughtsController.deleteThought);
 
-// api/thought/status
-router
-  .route('/status') //
+router // api/thought/status
+  .route('/status')
   .put(ThoughtsController.toggleThoughtStatus);
+
+router // api/thought/tag
+  .route('/tag')
+  .put(ThoughtsController.editTag);
 
 export default router;
