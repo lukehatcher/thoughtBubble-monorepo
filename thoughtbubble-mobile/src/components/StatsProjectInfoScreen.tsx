ed1189a084bd2af8d19f7d8deac1686d35066ddc
Luke Hatcher <lukehatcher98@gmail.com>
2021-05-04 22:08:57 -0700

    collect location data and display on colored graph

--- thoughtbubble-mobile/src/components/StatsProjectInfoScreen.tsx
+++ thoughtbubble-mobile/src/components/StatsProjectInfoScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { VictoryPie } from 'victory-native';
 import styled, { ThemeProvider } from 'styled-components/native';
@@ -7,20 +7,13 @@ import { StatsProjectInfoScreenProps } from '../interfaces/componentProps';
 import { RootState } from '../reducers/rootReducer';
 import { colors } from '../constants/colors';
 import { useDarkCheck } from '../hooks/useDarkCheck';
+import { locations } from '../constants/locations';
 
 const { darkMode, lightMode } = colors;
 
 export const StatsProjectInfoScreen: FC<StatsProjectInfoScreenProps> = function ({ route, navigation }) {
-  const isDarkMode = useDarkCheck();
-  const userProjectsData = useSelector((state: RootState) => state.userProjectData);
   const { projectId } = route.params;
-  const project = userProjectsData.find((i) => i.id === projectId);
-
-  const pieChartData = [
-    { x: 'mobile', y: 33 },
-    { x: 'VSCode', y: 66 },
-  ];
-
+  const isDarkMode = useDarkCheck();
   const theme = {
     // for styled-components ThemeProvider
     background: isDarkMode ? darkMode.background : lightMode.background,
@@ -30,19 +23,49 @@ export const StatsProjectInfoScreen: FC<StatsProjectInfoScreenProps> = function 
     textOnBackground: isDarkMode ? darkMode.textOnBackground : lightMode.textOnBackground,
     dp1: isDarkMode ? darkMode.dp1 : lightMode.background,
   };
+  const userProjectsData = useSelector((state: RootState) => state.userProjectData);
+
+  // tally up the number of thoughts added via vscode and mobile
+  const count = userProjectsData
+    .find((i) => i.id === projectId)
+    .projectThoughts.reduce(
+      (acc, curr) => {
+        if (curr.creationLocation === locations.MOBILE) acc.mobile += 1;
+        else acc.vscode += 1;
+        return acc;
+      },
+      { vscode: 0, mobile: 0 },
+    );
+
+  const pieChartData = [
+    { x: 'vscode', y: count.vscode },
+    { x: 'mobile', y: count.mobile },
+  ];
 
   return (
     <ThemeProvider theme={theme}>
       <MainContainer>
-        <Text>{JSON.stringify(project)}</Text>
+        {/* <Text>{JSON.stringify(project)}</Text> */}
         <PieChartHeader>where you spend your time</PieChartHeader>
-        <VictoryPie data={pieChartData} height={350} />
+        <VictoryPie
+          data={pieChartData}
+          style={{
+            data: {},
+            labels: {
+              fontSize: 17,
+              fill: theme.textOnBackground,
+            },
+          }}
+          height={350}
+          colorScale={'heatmap'}
+        />
       </MainContainer>
     </ThemeProvider>
   );
 };
 
 const MainContainer = styled.View`
+  flex: 1;
   background-color: ${(props) => props.theme.background};
 `;
 
