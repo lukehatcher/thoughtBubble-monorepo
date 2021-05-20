cc5b93f5cfb602a433b897ec58e46372f618d3a3
Luke Hatcher <lukehatcher98@gmail.com>
2021-05-20 13:32:06 -0700

    conditionally render dark/light native header icons

--- thoughtbubble-mobile/App.tsx
+++ thoughtbubble-mobile/App.tsx
@@ -18,6 +18,7 @@ import { RootState } from './src/reducers/rootReducer'; // type
 import store from './src/store';
 import { fetchUserInfoAction } from './src/actions/userInfoActions';
 import { fetchActivityDataAction } from './src/actions/fetchActivityAction';
+import { useDarkCheck } from './src/hooks/useDarkCheck';
 
 interface AppProps {}
 
@@ -25,9 +26,10 @@ const App: FC<AppProps> = () => {
   const loginStatus = useSelector((state: RootState) => state.storedUser);
 
   if (loginStatus.sub) {
+    const isDarkMode = useDarkCheck();
     return (
       <>
-        <StatusBar barStyle="light-content" />
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <AppTabsNavigator />
       </>
     );
