b88b0ff092618e778c3ba04b628e0ccf75e793f0
Luke Hatcher <lukehatcher98@gmail.com>
2021-05-25 10:29:58 -0700

    input field limits project name length to 25 charcters

--- thoughtbubble-mobile/src/components/AddProjectModal.tsx
+++ thoughtbubble-mobile/src/components/AddProjectModal.tsx
@@ -1,118 +1,102 @@
 import React, { useState } from 'react';
-import { Modal, View, Alert, StyleSheet } from 'react-native';
+import { Modal, View, StyleSheet } from 'react-native';
 import { useDispatch } from 'react-redux';
-import { Button, IconButton, TextInput } from 'react-native-paper';
+import { Button, IconButton, TextInput, HelperText } from 'react-native-paper';
 import { addProjectAction } from '../actions/projectActions';
 import { colors } from '../constants/colors';
 import { AddProjectModalProps } from '../interfaces/componentProps';
 import { useDarkCheck } from '../hooks/useDarkCheck';
+import styled from 'styled-components/native';
+
+const { darkMode, lightMode } = colors;
 
 export const AddProjectModal: React.FC<AddProjectModalProps> = function ({ addProjModalView, setAddProjModalView }) {
   const [input, setInput] = useState('');
   const dispatch = useDispatch();
-  const theme = useDarkCheck();
+  const isDarkMode = useDarkCheck();
 
-  const useTheme = (name: string) => (theme ? stylesDark[name] : stylesLight[name]);
-
-  const handleProjectAddition = function (projectName: string) {
-    setInput('');
-    if (!projectName) {
-      Alert.alert('invalid input');
+  const handleProjectAddition = function (): void {
+    const newProjectName = input.trim();
+    if (!newProjectName || newProjectName.length > 25) {
       return;
     }
-    dispatch(addProjectAction(projectName));
+    setInput('');
+    setAddProjModalView(false);
+    dispatch(addProjectAction(newProjectName));
+  };
+
+  const inputIsTooLong = function (): boolean {
+    return input.trim().length > 25;
   };
 
   return (
-    <>
-      <Modal animationType="slide" visible={addProjModalView}>
-        <View style={useTheme('modal')}>
-          <TextInput
-            mode="outlined"
-            label="new project"
-            value={input}
-            onChangeText={(input) => setInput(input)}
-            placeholder="Add a new project"
-            multiline
-            style={useTheme('textInput')}
-            theme={{
-              colors: {
-                primary: theme ? colors.darkMode.primary : colors.lightMode.primary,
-                text: theme ? colors.darkMode.textOnSurface : colors.lightMode.textOnBackground,
-                placeholder: theme ? `${colors.darkMode.textOnSurface}87` : `${colors.lightMode.textOnBackground}87`,
-              },
-            }}
-            keyboardAppearance="dark"
-          />
-          <Button
-            mode="contained"
-            icon="folder-upload"
-            color={theme ? colors.darkMode.primary : colors.lightMode.primary}
-            onPress={() => {
-              setAddProjModalView(false);
-              handleProjectAddition(input.trim());
-            }}
-            style={useTheme('btn')}
-          >
-            add project
-          </Button>
-          <IconButton
-            icon="close"
-            size={50}
-            color={theme ? colors.darkMode.primary : colors.lightMode.primary}
-            style={sharedStyles.closeBtn}
-            onPress={() => {
-              setAddProjModalView(false);
-              setInput('');
-            }}
-          />
-        </View>
-      </Modal>
-    </>
+    <Modal animationType="slide" visible={addProjModalView}>
+      <ModalContainer>
+        <HelperText type="error" visible={inputIsTooLong()} style={{ marginBottom: 15 }}>
+          Error: Project name must be less than 26 characters
+        </HelperText>
+        <TextInput
+          mode="outlined"
+          label="new project"
+          value={input}
+          onChangeText={(input) => setInput(input)}
+          multiline
+          keyboardAppearance="dark"
+          style={[
+            styles.textInput,
+            {
+              color: isDarkMode ? darkMode.textOnBackground : lightMode.textOnBackground,
+              backgroundColor: isDarkMode ? darkMode.background : lightMode.background,
+            },
+          ]}
+          theme={{
+            colors: {
+              primary: isDarkMode ? darkMode.primary : lightMode.primary,
+              text: isDarkMode ? darkMode.textOnSurface : lightMode.textOnBackground,
+              placeholder: isDarkMode ? `${darkMode.textOnSurface}87` : `${lightMode.textOnBackground}87`,
+            },
+          }}
+        />
+        <Button
+          mode="contained"
+          icon="folder-upload"
+          color={isDarkMode ? darkMode.primary : lightMode.primary}
+          onPress={() => handleProjectAddition()}
+          style={styles.btn}
+        >
+          add project
+        </Button>
+        <IconButton
+          icon="close"
+          size={50}
+          color={isDarkMode ? darkMode.primary : lightMode.primary}
+          style={styles.closeBtn}
+          onPress={() => {
+            setAddProjModalView(false);
+            setInput('');
+          }}
+        />
+      </ModalContainer>
+    </Modal>
   );
 };
 
-const sharedStyles = StyleSheet.create({
+const ModalContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.background};
+`;
+
+const styles = StyleSheet.create({
+  textInput: {
+    width: 250,
+    marginBottom: 10,
+  },
   closeBtn: {
     position: 'absolute',
     top: 50,
     right: 16,
   },
-});
-
-const stylesDark = StyleSheet.create({
-  modal: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.darkMode.background,
-  },
-  textInput: {
-    color: 'white',
-    backgroundColor: colors.darkMode.dp1,
-    width: 250,
-    marginBottom: 10,
-  },
-  btn: {
-    marginBottom: 10,
-    // width: 250,
-  },
-});
-
-const stylesLight = StyleSheet.create({
-  modal: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.lightMode.background,
-  },
-  textInput: {
-    backgroundColor: colors.lightMode.background,
-    width: 250,
-    marginBottom: 10,
-  },
-  btn: {
-    marginBottom: 10,
-    // width: 250,
-  },
+  btn: { marginTop: 10 },
 });
