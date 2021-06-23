8d52095548575c8bbc845b4c25ca756e98a6ae07
Luke Hatcher <lukehatcher98@gmail.com>
2021-06-22 23:50:17 -0700

    fix remove splash screen on load bug

--- thoughtbubble-mobile/src/screens/LoginScreen.tsx
+++ thoughtbubble-mobile/src/screens/LoginScreen.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, TouchableHighlight } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { LoginScreenProps } from '../interfaces/screenProps';
 import { TextTB } from '../components/Text';
 import { _loginGitHub } from '../utils/auth';
 import { darkMode } from '../constants/colors';
+import RNBootSplash from 'react-native-bootsplash';
 
 export const LoginScreen: React.FC<LoginScreenProps> = () => {
+  useEffect(() => {
+    (async () => {
+      await RNBootSplash.hide();
+    })();
+  }, []);
+
   return (
     <>
       <View style={styles.container}>
