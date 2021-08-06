8bb2fd95e4e6d5772dd2950aaa912be668ebbb2a
Luke Hatcher <lukehatcher98@gmail.com>
2021-08-06 16:33:15 -0700

    add vector icon types and update styled component defaultTheme type

---
+++ thoughtbubble-mobile/src/types/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+// extend them
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background: string;
+    primary: string;
+    primaryVariant: string;
+    secondary: string;
+    textOnBackground: string;
+    dp1: string;
+    textOnError: string;
+    error: string;
+    borderColor: string;
+  }
+}
