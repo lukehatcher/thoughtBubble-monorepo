260dd2a155a559eecf064331d60d17a9086ccdb2
Luke Hatcher <lukehatcher98@gmail.com>
2021-09-27 22:53:57 -0700

    close local vscode auth server after auth and add redirect landing page skeleton

--- thoughtbubble-vscode/src/auth.ts
+++ thoughtbubble-vscode/src/auth.ts
@@ -1,27 +1,54 @@
 import * as vscode from 'vscode';
 import * as polka from 'polka';
+import { Server } from 'http';
 import { StateManager } from './stateManager';
-import { createBrotliCompress } from 'zlib';
 
+// TODO: add env
 const authEndpoint = 'http://localhost:3001/auth/github/vscode';
-const vscodeServerPort = 7777;
+const PORT = 7777;
 
 export const authenticate = (cb: () => void) => {
-  const polkaServer = polka();
+  const app = polka();
 
-  polkaServer.get('/auth/:authToken', async (req, res) => {
+  app.get('/auth/:authToken', async (req, res) => {
     const { authToken } = req.params;
     if (!authToken) {
-      res.end('<h1>error, something went wrong</h1>');
+      res.end('<h1>oh no! looks like something went wrong... please close the tab and retry</h1>');
     }
-    res.end('<h1>auth was sucessful</h1>');
+    res.end(
+      `
+          <div>
+            <div class="container">
+              <h1>ThoughtBubble</h1>
+              <h3>authorization was successful, you can close this tab and return to VSCode</h3>
+              <a href="https://github.com/lukehatcher/thoughtBubble-monorepo">visit repo here</a>
+            </div>
+            <style>
+              .container {
+                text-align: center;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                font-family: SF Pro Text, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif;
+              }
+            </style>
+            <script>
+              // this.document.location = 'vscode://';
+              // window.close();
+            </script>
+          </div>
+      `
+    );
+    // res.end('<script> this.document.location = "vscode://"; </script>');
+    // res.end('<script> this.document.location = "vscode://"; window.close(); </script>'); // this version does not redirect you to vscode, it just closes out
     await StateManager.setToken(authToken);
-    // send a message back to the view with the token in it so the view can change from logged out to logged in state
+    // this cb sends a vscode-message back to the view with the access token in it so the view can change state from logged out to logged in
     cb();
-    // (polkaServer as any).close();
+    (app.server as Server).close();
   });
 
-  polkaServer.listen(vscodeServerPort, (err: Error) => {
+  app.listen(PORT, (err: Error) => {
     if (err) {
       vscode.window.showErrorMessage(err.message);
     } else {
