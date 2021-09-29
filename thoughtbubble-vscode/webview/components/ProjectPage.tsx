09f67d57d070bbce882fed442052e4be8567dc80
Luke Hatcher <lukehatcher98@gmail.com>
2021-09-29 13:48:17 -0700

    implements useOrderProjects hook

--- thoughtbubble-vscode/webview/components/ProjectPage.tsx
+++ thoughtbubble-vscode/webview/components/ProjectPage.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
-// import { useOrderProjects } from '../hooks/useOrderProjects';
+import { useOrderProjects } from '../hooks/useOrderProjects';
 import { RootState } from '../reducers/rootReducer';
 import { LoginButton } from './LoginButton';
 import { ProjectCard } from './ProjectCard';
 import { ProjectsHeader } from './ProjectsHeader';
 
 export const ProjectPage: FC = () => {
-  const userProjects = useSelector((state: RootState) => state.userProjectData);
-  // const userProjects = useOrderProjects();
+  const userProjects = useOrderProjects();
   const token = useSelector((state: RootState) => state.token);
+
+  // can the people and the runners and then we can go
+  // const userInfo = useSelector((state: RootState) => state.userInfo);
+  // console.log('userInfo: ', userInfo);
 
   return (
     <>
       {!token ? (
         <>
-          {' '}
           <LoginButton />
         </>
       ) : (
--- thoughtbubble-vscode/webview/hooks/useOrderProjects.ts
+++ thoughtbubble-vscode/webview/hooks/useOrderProjects.ts
@@ -1,29 +1,30 @@
-// import { useSelector } from "react-redux";
-// import { ProjectShape } from "../interfaces/interfaces";
-// import { RootState } from "../reducers/rootReducer";
+import { useSelector } from 'react-redux';
+import { Directions, OrderTypes } from '../constants/orders';
+import { ProjectShape } from '../interfaces/interfaces';
+import { RootState } from '../reducers/rootReducer';
 
-// export const useOrderProjects = function (): ProjectShape[] {
-//   const userInfoData = useSelector((state: RootState) => state.userInfo);
-//   let userProjectsData = useSelector((state: RootState) => state.userProjectData);
-//   if (userProjectsData.length <= 1) return userProjectsData;
-//   const { projectDirection, projectOrder } = userInfoData;
-//   // collect pins and sort by pin date (lates pin at top)
-//   const pins = userProjectsData.filter((proj) => proj.pinned).sort((a, b) => b.pinDate.localeCompare(a.pinDate));
-//   // remove pins
-//   userProjectsData = userProjectsData.filter((i) => !i.pinned);
+export const useOrderProjects = function (): ProjectShape[] {
+  const userInfoData = useSelector((state: RootState) => state.userInfo);
+  let userProjectsData = useSelector((state: RootState) => state.userProjectData);
+  if (userProjectsData.length <= 1) return userProjectsData;
+  const { projectDirection, projectOrder } = userInfoData;
+  // collect pins and sort by pin date (lates pin at top)
+  const pins = userProjectsData.filter((proj) => proj.pinned).sort((a, b) => b.pinDate.localeCompare(a.pinDate));
+  // remove pins
+  userProjectsData = userProjectsData.filter((i) => !i.pinned);
 
-//   if (projectOrder === OrderTypes.LAST_UPDATED) {
-//     // sort by last update (api returns this by default)
-//     userProjectsData = [...userProjectsData.sort((a, b) => b.lastUpdatedDate.localeCompare(a.lastUpdatedDate))]; // oldest at top: ;
-//   } else if (projectOrder === OrderTypes.ALPHABETICAL) {
-//     // sort projects alphabetically
-//     userProjectsData = [...userProjectsData.sort((a, b) => a.projectName.localeCompare(b.projectName))];
-//   } else {
-//     // sort projects by size (number or thoughts in them)
-//     userProjectsData = [...userProjectsData.sort((a, b) => b.projectThoughts.length - a.projectThoughts.length)];
-//   }
-//   // format in ascending or descending order, + add pins to front
-//   return projectDirection === Directions.ASC
-//     ? [...pins, ...userProjectsData.reverse()]
-//     : [...pins, ...userProjectsData];
-// };
+  if (projectOrder === OrderTypes.LAST_UPDATED) {
+    // sort by last update (api returns this by default)
+    userProjectsData = [...userProjectsData.sort((a, b) => b.lastUpdatedDate.localeCompare(a.lastUpdatedDate))]; // oldest at top: ;
+  } else if (projectOrder === OrderTypes.ALPHABETICAL) {
+    // sort projects alphabetically
+    userProjectsData = [...userProjectsData.sort((a, b) => a.projectName.localeCompare(b.projectName))];
+  } else {
+    // sort projects by size (number or thoughts in them)
+    userProjectsData = [...userProjectsData.sort((a, b) => b.projectThoughts.length - a.projectThoughts.length)];
+  }
+  // format in ascending or descending order, + add pins to front
+  return projectDirection === Directions.ASC
+    ? [...pins, ...userProjectsData.reverse()]
+    : [...pins, ...userProjectsData];
+};
